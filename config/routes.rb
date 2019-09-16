a5244ca77ce8ef55784e073db2a5199ee0263ea7
Stefanie Holbrook <stefholbrook@gmail.com>
2019-09-16 09:59:56 -0700

    Tests and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+require "api_constraints"
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # maps to controllers/api directory for api.marketplace_api.test subdomain and the starting path to be root
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    # maps to controllers/api/v1 directory
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+
+    end
+  end
 end
